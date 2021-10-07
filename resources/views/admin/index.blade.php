c612b894b61db5bdb0e53c576d808649a0211e7d
kaylarahma <kaylarahmanisa83@gmail.com>
2021-10-07 16:39:00 +0700

    nambah admin

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.admin')
+
+@section('content')
+<h1>Halaman Admin</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -19,16 +19,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function() {
     return view('index');
-})->name('index');
+});
 
 Route::get('about', function() {
     return view('about');
-})->name('about');
+});
 
 Route::get('post', function() {
     return view('post');
-})->name('post');
+});
 
 Route::get('contact', function() {
     return view('contact');
-})->name('contact');
+});
+
+Route::prefix('admin')->group(function() {
+    Route::get('/', function() {
+        return view('admin.index');
+    });
+});
+
