96aa36ee391170571f945b68fd5a5eb996ca208d
kaylarahma <kaylarahmanisa83@gmail.com>
2021-10-08 16:01:15 +0700

    revisi admin

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -42,17 +42,27 @@
   @include('include.admin.sidebar')
 
   <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Dashboard</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard v1</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
   @yield('content')
+  </div>
   <!-- /.content-wrapper -->
   @include('include.admin.footer')
-
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
 
 <!-- jQuery -->
 <script src="{{asset('assets/admin/plugins/jquery/jquery.min.js')}}"></script>
