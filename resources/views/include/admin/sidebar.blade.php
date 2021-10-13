9dab0a30b95690d72ed40c836f473d1709bac683
kaylarahma <kaylarahmanisa83@gmail.com>
2021-10-13 16:47:08 +0700

    Controller

--- resources/views/include/admin/sidebar.blade.php
+++ resources/views/include/admin/sidebar.blade.php
@@ -13,7 +13,7 @@
           <img src="{{asset('assets/admin/dist/img/avatar2.png')}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">{{Auth::user()->name}}</a>
+          {{-- <a href="#" class="d-block">{{Auth::user()->name}}</a> --}}
         </div>
       </div>
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -42,3 +43,5 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('book', BookController::class);
